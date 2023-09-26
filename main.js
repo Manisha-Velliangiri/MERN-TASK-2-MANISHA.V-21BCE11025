65f3c422ad66127331236f657ba6f7dd4d16bbdd
Manisha.V <128169122+Manisha-Velliangiri@users.noreply.github.com>
2023-09-26 15:07:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    
+
+    if (username=="Manisha" ) {
+        alert("Registration successful");
+    }
+    else{
+        alert("Registration failed");
+    }
+    
+}
